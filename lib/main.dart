1c53dd1893eefee7626dd9273afdab7503a591bb
krishipaladiya <krishipaladiya@gmail.com>
2023-02-15 11:52:13 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:fashion_gallery/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: homepage(),
+    ),
+  );
+}
